8dac142eae6fa2ac0519a006dc6abe16bd8692d3
Viktar Svintsitski <jetalai3@gmail.com>
2023-01-22 21:47:26 +0100

    fix: fix sql script to create more records

--- create_table_with_data.sql
+++ create_table_with_data.sql
@@ -9,4 +9,7 @@ CREATE TABLE IF NOT EXISTS public.users
 );
 
 INSERT INTO public.users(id, login, password, age, is_deleted) VALUES
-('3e9d36a0-c349-4730-adc5-c4d7654956bc', 'user1', 'password1', 21, false);
+('3e9d36a0-c349-4730-adc5-c4d7654956bc', 'user1', 'password1', 21, false),
+('78e230c6-b00c-4b62-b068-a3b163440073', 'user2', 'password2', 22, false),
+('2d44ebea-d445-4594-b888-8ebc63bbde53', 'user3', 'password3', 23, false),
+('f46c8b21-f61c-411d-85ae-9f8e7f43f63b', 'user4', 'password4', 24, false);
